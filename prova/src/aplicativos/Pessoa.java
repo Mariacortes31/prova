db823073e003765e401fd29cb176bfcf056ba803
Mariacortes31 <mariapaula.mpc17@gmail.com>
2022-09-11 17:34:47 -0300

    first commit

---
+++ prova/src/aplicativos/Pessoa.java
@@ -0,0 +1,19 @@
+package aplicativos;
+
+public class Pessoa { 
+    String cpf;
+    String nome;
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
+      
